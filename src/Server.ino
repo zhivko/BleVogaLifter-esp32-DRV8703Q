753e67688065db8c1e1bd4ec18391c337791eb8f
zhivko <klemen.zivkovic@gmail.com>
2018-06-28 16:13:01 +0200

    First SPI communication from ESP32 to DRV8703Q

--- src/Server.ino
+++ src/Server.ino
@@ -38,6 +38,7 @@ SPIClass * vspi = NULL;
 #define LEDC_CHANNEL_2 2
 #define LEDC_CHANNEL_3 3
 
+#define SS1 33
 
 int brightness = 0; // how bright the LED is
 int fadeAmount = 1; // how many points to fade the LED by
@@ -64,10 +65,10 @@ void setup()
     Serial.println("initialise vspi with default pins 2...");
     // VSPI - SCLK = 18, MISO = 19, MOSI = 23, SS = 5
     // begin(int8_t sck=-1, int8_t miso=-1, int8_t mosi=-1, int8_t ss=-1);
-    vspi->begin(18,19,23,33);
+    vspi->begin(18,19,23,SS1);
     Serial.println("initialise vspi with default pins 3...");
 
-    pinMode(33, OUTPUT); //VSPI SS
+    pinMode(SS1, OUTPUT); //VSPI SS
     delay(10);
 
     Serial.println("initialise ledc...");
@@ -128,21 +129,108 @@ void setup()
 }
 
 int value = 0;
+int doSpi = 0;
 
 void testSpi()
 {
-  usleep(10);
+  //SPI_MODE0, ..., SPI_MODE3
+  vspi->setDataMode(SPI_MODE0);
+  vspi->setHwCs(false);
+
+  usleep(1);
+  digitalWrite(SS1, LOW);
   // SPI WRITE
   vspi->beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE0));
-  //byte stuff = 0b11001100;
-  //hspi->transfer(stuff);
-  digitalWrite(33, LOW);
-  toTransfer = 0b11001100 << 8;
-  toTransfer |= 0b11001100 ;
-  vspi->transfer16(toTransfer);
-  digitalWrite(33, HIGH);
+
+  // http://www.ti.com/product/drv8702-q1?qgpn=drv8702-q1
+  // datasheet: http://www.ti.com/lit/gpn/drv8702-q1
+  // page 42
+
+/*
+  uint16_t data = 0;
+  uint16_t data_read = 1000000000000000B;
+  uint16_t data_address = 0001000000000000B;
+
+  uint16_t data_maincontrol_lock = 0001100000000000B;
+
+  uint16_t data_maincontrol_in1 = 0000010000000000B;
+  uint16_t data_maincontrol_in2 = 0000001000000000B;
+
+*/
+  byte data_read = B10000000;  // READ OPERATION
+  byte data_address = B00010000; // ADDRES 02x MAIN REGISTER
+  byte data = data_read | data_address;
+  byte lowbyte = B0;
+  uint16_t data_int = data << 8 | lowbyte;
+
+  uint16_t reply = vspi->transfer16(data_int);  // should return 0x18 B00011000
   vspi->endTransaction();
-  usleep(10);
+  digitalWrite(SS1, HIGH);
+  usleep(1);
+  if(reply<<8 == B00011000)
+  {
+    Serial.println("DRV8703Q Not locked");
+  }
+
+  usleep(1);
+  digitalWrite(SS1, LOW);
+  // SPI WRITE
+  vspi->beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE0));
+  // fault bytes:
+  uint8_t FAULT_FAULT = B1 << 7;     // FAULT R 0b Logic OR of the FAULT status register excluding the OTW bit
+  uint8_t FAULT_WDFLT = B1 << 6;     // WDFLT R 0b Watchdog time-out fault
+  uint8_t FAULT_GDF = B1 << 5;       // GDF R 0b Indicates gate drive fault condition
+  uint8_t FAULT_OCP = B1 << 4;       // OCP R 0b Indicates VDS monitor overcurrent fault condition
+  uint8_t FAULT_VM_UVFL = B1 << 3;   // VM_UVFL R 0b Indicates VM undervoltage lockout fault condition
+  uint8_t FAULT_VCP_UVFL = B1 << 2;  // VCP_UVFL R 0b Indicates charge-pump undervoltage fault condition
+  uint8_t FAULT_OTSD = B1 << 1;      // OTSD R 0b Indicates overtemperature shutdown
+  uint8_t FAULT_OTW = B1 << 0;       // OTW R 0b Indicates overtemperature warning
+
+  data_read = B10000000;  // READ OPERATION
+  data_address = B00000000; // ADDRES 0x FAULT REGISTER
+  data = data_read | data_address;
+  lowbyte = B0;
+  data_int = data << 8 | lowbyte;
+
+  reply = vspi->transfer16(data_int);  // should return 0x18
+  vspi->endTransaction();
+  digitalWrite(SS1, HIGH);
+  usleep(1);
+  Serial.println(reply);
+
+  if(reply & FAULT_FAULT > 0)
+  {
+    Serial.println("fault");
+  }
+  if(reply & FAULT_WDFLT > 0)
+  {
+    Serial.println("Watchdog time-out fault");
+  }
+  if(reply & FAULT_GDF > 0)
+  {
+    Serial.println("Gate drive fault");
+  }
+  if(reply & FAULT_OCP > 0)
+  {
+    Serial.println("VDS monitor overcurrent fault");
+  }
+  if(reply & FAULT_VM_UVFL > 0)
+  {
+    Serial.println("VM undervoltage lockout fault");
+  }
+  if(reply & FAULT_VCP_UVFL > 0)
+  {
+    Serial.println("Charge-pump undervoltage fault");
+  }
+  if(reply & FAULT_OTSD > 0)
+  {
+    Serial.println("Overtemperature shutdown");
+  }
+  if(reply & FAULT_OTW > 0)
+  {
+    Serial.println("Overtemperature warning");
+  }
+
 }
 
 void checkWifiClient()
@@ -193,15 +281,7 @@ void checkWifiClient()
          if (currentLine.endsWith("GET /L")) {
            //Serial.println("\nSPI Start.");
            //digitalWrite(33, LOW);
-           testSpi();
-           usleep(100);
-           testSpi();
-           usleep(100);
-           testSpi();
-           usleep(100);
-           testSpi();
-           usleep(100);
-
+           doSpi = 1;
            //Serial.println("SPI Done.");
          }
          if (currentLine.endsWith("GET /UP")) {
@@ -233,7 +313,14 @@ void checkWifiClient()
 
 void loop(){
   //testSpi();
-  checkWifiClient();
+  //checkWifiClient();
+  if(doSpi % 10000 == 0)
+  {
+    Serial.print(doSpi);
+    Serial.println(" doSPI");
+    testSpi();
+  }
+  doSpi++;
   ArduinoOTA.handle();
-  usleep(1000);
+  usleep(10);
 }

9c5b519c3168fb3b0aa4b623a9a581a620c0c3e1
zhivko <klemen.zivkovic@gmail.com>
2018-07-02 14:26:31 +0200

    changes

--- src/Server.ino
+++ src/Server.ino
@@ -1,124 +1,204 @@
-#include "ESPAsyncTCP.h"
-#include "ESPAsyncWebServer.h"
+#include <WiFi.h>
+#include <FS.h>
+#include "SPIFFS.h"
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <SPI.h>
+#include <ArduinoOTA.h>
 
+
+const char* ssid = "AndroidAP";
+const char* password =  "Doitman1";
 AsyncWebServer server(80);
-AsyncWebSocket ws("/ws"); // access at ws://[esp ip]/ws
-AsyncEventSource events("/events"); // event source (Server-Sent events)
 
-const char* ssid = "AndroidAp";
-const char* password = "Doitman1";
-const char* http_username = "admin";
-const char* http_password = "admin";
 
-//flag to use from web update to reboot the ESP
-bool shouldReboot = false;
+static const int spiClk = 1000000; // 1 MHz
+uint16_t toTransfer;
+//uninitalised pointers to SPI objects
+SPIClass * vspi = NULL;
 
-void onRequest(AsyncWebServerRequest *request){
-  //Handle Unknown Request
-  request->send(404);
+// use 13 bit precission for LEDC timer
+#define LEDC_TIMER_10_BIT  10
+// use 5000 Hz as a LEDC base frequency
+#define LEDC_BASE_FREQ 5000
+// fade LED PIN (replace with LED_BUILTIN constant for built-in LED)
+#define PWM1_PIN 12
+#define PWM2_PIN 14
+#define PWM3_PIN 26
+#define PWM4_PIN 27
+// use first channel of 16 channels (started from zero)
+#define LEDC_CHANNEL_0 0
+#define LEDC_CHANNEL_1 1
+#define LEDC_CHANNEL_2 2
+#define LEDC_CHANNEL_3 3
+#define SS1 33
+#define pwmValueMax  2000
+
+int brightness = 0; // how bright the LED is
+int fadeAmount = 1; // how many points to fade the LED by
+
+String status1;
+String status2;
+
+
+void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 1024) {
+  // calculate duty, 8191 from 2 ^ 13 - 1
+  uint32_t duty = (8191 / valueMax) * _min(value, valueMax);
+
+  // write duty to LEDC
+  ledcWrite(channel, duty);
 }
 
-void onBody(AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total){
-  //Handle body
+
+void testSpi()
+{
+  //SPI_MODE0, ..., SPI_MODE3
+  vspi->setDataMode(SPI_MODE1);
+  vspi->setHwCs(false);
+
+  usleep(1);
+  digitalWrite(SS1, LOW);
+  // SPI WRITE
+  vspi->beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE1));
+
+  // http://www.ti.com/product/drv8702-q1?qgpn=drv8702-q1
+  // datasheet: http://www.ti.com/lit/gpn/drv8702-q1
+  // page 42
+
+
+  byte data_read = B10000000;  // READ OPERATION
+  byte data_address = B00010000; // ADDRES 02x MAIN REGISTER
+  byte data = data_read | data_address;
+  byte lowbyte = B0;
+  uint16_t data_int = data << 8 | lowbyte;
+
+  uint16_t reply = vspi->transfer16(data_int);  // should return 0x18 B00011000
+  vspi->endTransaction();
+  digitalWrite(SS1, HIGH);
+  usleep(1);
+  if(reply == B00011000)
+  {
+    status1 = "DRV8703Q is ON (Not locked).";
+  }
+
+  usleep(1);
+  digitalWrite(SS1, LOW);
+  // SPI WRITE
+  vspi->beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE1));
+  // fault bytes:
+  uint8_t FAULT_FAULT = B1 << 7;     // FAULT R 0b Logic OR of the FAULT status register excluding the OTW bit
+  uint8_t FAULT_WDFLT = B1 << 6;     // WDFLT R 0b Watchdog time-out fault
+  uint8_t FAULT_GDF = B1 << 5;       // GDF R 0b Indicates gate drive fault condition
+  uint8_t FAULT_OCP = B1 << 4;       // OCP R 0b Indicates VDS monitor overcurrent fault condition
+  uint8_t FAULT_VM_UVFL = B1 << 3;   // VM_UVFL R 0b Indicates VM undervoltage lockout fault condition
+  uint8_t FAULT_VCP_UVFL = B1 << 2;  // VCP_UVFL R 0b Indicates charge-pump undervoltage fault condition
+  uint8_t FAULT_OTSD = B1 << 1;      // OTSD R 0b Indicates overtemperature shutdown
+  uint8_t FAULT_OTW = B1 << 0;       // OTW R 0b Indicates overtemperature warning
+
+  data_read = B10000000;  // READ OPERATION
+  data_address = B00000000; // ADDRES 0x FAULT REGISTER
+  data = data_read | data_address;
+  lowbyte = B0;
+  data_int = data << 8 | lowbyte;
+
+  reply = vspi->transfer16(data_int);  // should return 0x18
+  vspi->endTransaction();
+  digitalWrite(SS1, HIGH);
+  usleep(1);
+  //Serial.println(reply);
+
+  if((reply & FAULT_FAULT) > 0)
+  {
+    //Serial.println("fault");
+  }
+  if((reply & FAULT_WDFLT) > 0)
+  {
+    status1 = "Watchdog time-out fault";
+  }
+  if((reply & FAULT_GDF) > 0)
+  {
+    status1 = "Gate drive fault";
+  }
+  if((reply & FAULT_OCP) > 0)
+  {
+    status1 = "VDS monitor overcurrent fault";
+  }
+  if((reply & FAULT_VM_UVFL) > 0)
+  {
+    status1 = "VM undervoltage lockout fault";
+  }
+  if((reply & FAULT_VCP_UVFL) > 0)
+  {
+    status1 = "Charge-pump undervoltage fault";
+  }
+  if((reply & FAULT_OTSD) > 0)
+  {
+    status1 = "Overtemperature shutdown";
+  }
+  if((reply & FAULT_OTW) > 0)
+  {
+    status1 = "Overtemperature warning";
+  }
 }
 
-void onUpload(AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final){
-  //Handle upload
+void pwmUp(){
+  if(brightness<=(pwmValueMax-1))
+  {
+    brightness = brightness + 1;
+    ledcAnalogWrite(LEDC_CHANNEL_0, brightness, pwmValueMax);
+  }
 }
 
-void onEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
-  //Handle WebSocket event
+void pwmDown()
+{
+  if(brightness>=1)
+  {
+   brightness = brightness - 1;
+   ledcAnalogWrite(LEDC_CHANNEL_0, brightness, pwmValueMax);
+  }
+  Serial.print("duty= ");
+  Serial.println(brightness);
 }
 
 void setup(){
   Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
+
   WiFi.begin(ssid, password);
-  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.printf("WiFi Failed!\n");
-    return;
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
   }
 
-  // attach AsyncWebSocket
-  ws.onEvent(onEvent);
-  server.addHandler(&ws);
+  Serial.println(WiFi.localIP());
 
-  // attach AsyncEventSource
-  server.addHandler(&events);
-
-  // respond to GET requests on URL /heap
-  server.on("/heap", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/plain", String(ESP.getFreeHeap()));
+  server.on("/status", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("/status");
+    testSpi();
+    request->send(200, "text/plain", status1);
+  });
+  server.on("/up", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("/up");
+    pwmUp();
+    request->send(200, "text/plain", "Hello World");
+  });
+  server.on("/down", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("/down");
+    request->send(200, "text/plain", "Hello World");
   });
 
-  // upload a file to /upload
-  server.on("/upload", HTTP_POST, [](AsyncWebServerRequest *request){
-    request->send(200);
-  }, onUpload);
+  if(!SPIFFS.begin()){
+      Serial.println("SPIFFS Mount Failed");
+      return;
+  }  
 
-  // send a file when /index is requested
-  server.on("/index", HTTP_ANY, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.htm");
-  });
-
-  // HTTP basic authentication
-  server.on("/login", HTTP_GET, [](AsyncWebServerRequest *request){
-    if(!request->authenticate(http_username, http_password))
-        return request->requestAuthentication();
-    request->send(200, "text/plain", "Login Success!");
-  });
-
-  // Simple Firmware Update Form
-  server.on("/update", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/html", "<form method='POST' action='/update' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Update'></form>");
-  });
-  server.on("/update", HTTP_POST, [](AsyncWebServerRequest *request){
-    shouldReboot = !Update.hasError();
-    AsyncWebServerResponse *response = request->beginResponse(200, "text/plain", shouldReboot?"OK":"FAIL");
-    response->addHeader("Connection", "close");
-    request->send(response);
-  },[](AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final){
-    if(!index){
-      Serial.printf("Update Start: %s\n", filename.c_str());
-      Update.runAsync(true);
-      if(!Update.begin((ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000)){
-        Update.printError(Serial);
-      }
-    }
-    if(!Update.hasError()){
-      if(Update.write(data, len) != len){
-        Update.printError(Serial);
-      }
-    }
-    if(final){
-      if(Update.end(true)){
-        Serial.printf("Update Success: %uB\n", index+len);
-      } else {
-        Update.printError(Serial);
-      }
-    }
-  });
-
-  // attach filesystem root at URL /fs
-  server.serveStatic("/fs", SPIFFS, "/");
-
-  // Catch-All Handlers
-  // Any request that can not find a Handler that canHandle it
-  // ends in the callbacks below.
-  server.onNotFound(onRequest);
-  server.onFileUpload(onUpload);
-  server.onRequestBody(onBody);
+  server.serveStatic("/", SPIFFS, "/www/");
 
   server.begin();
+  ArduinoOTA.begin();
 }
 
 void loop(){
-  if(shouldReboot){
-    Serial.println("Rebooting...");
-    delay(100);
-    ESP.restart();
-  }
-  static char temp[128];
-  sprintf(temp, "Seconds since boot: %u", millis()/1000);
-  events.send(temp, "time"); //send event "time"
+  ArduinoOTA.handle();
+  usleep(10);
 }
